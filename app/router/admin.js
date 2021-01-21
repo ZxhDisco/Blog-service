e4694e04cec166d341adf130749c2355d5c2ae4a
ZxhDisco <971696823@qq.com>
2021-01-21 17:19:46 +0800

    v0.1.0

---
+++ app/router/admin.js
@@ -0,0 +1,20 @@
+module.exports = app =>{
+    const {router,controller}=app
+    // 声明并引入中间件。
+    // const adminauth =  app.middleware.adminauth()
+    router.get("/admin/index",controller.admin.main.index)
+    router.post("/admin/checkLogin",controller.admin.main.checkLogin)
+    // 需要添加路由守卫
+    router.get("/admin/getTypeInfo",controller.admin.main.getTypeInfo)
+    router.post("/admin/addArticle",controller.admin.main.addArticle)
+    router.post("/admin/updateArticle",controller.admin.main.updateArticle)
+    router.get("/admin/getArticleList",controller.admin.main.getArticleList)
+    router.get("/admin/delArticle/:id",controller.admin.main.delArticle)
+    router.get("/admin/getArticleById/:id",controller.admin.main.getArticleById)
+
+
+
+
+
+
+}
